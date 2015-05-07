3235eeb469394da0876ed9fe1765bc15ef08af54
Han Gong <gong@fedoraproject.org>
2015-05-07 21:24:46 +0100

    1.fix a bug. 2.add better visualisation

--- Demo.m
+++ Demo.m
@@ -3,8 +3,9 @@ load data/GRA31
 load data/surfs1995
 
 %%%% THESE CAN BE ADJUSTED TO TEST %%%
-nSensors = 3;
-nSurfaces = 6;
+nSensors = 3; % number of sensors
+nSurfaces = 9; % number of surfaces reflectances to solve
+nLight = 3; % number of ambient light basis (nLight < nSurfaces)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if nSensors < 3 || nSensors > 4
@@ -23,27 +24,43 @@ if nSensors ==4
 end
 
 % Select some random bases
-lightB = GRA31(:,randperm(size(GRA31, 2), nSensors)); % ambient light 31xN
+lightB = GRA31(:,randperm(size(GRA31, 2), nLight)); % ambient light 31xN
 surfB = surfs1995(:,randperm(size(surfs1995, 2), nSensors-1)); % reflectance 31xP
 
 % Create an illuminant and some surfaces from the basis functions with random weights
 trueIllum = lightB*rand(size(lightB, 2),1); % 31x1
-trueSurfArray = surfB*rand(size(surfB, 2),nSurfaces); % 31xn
+trueSurfA = surfB*rand(size(surfB, 2),nSurfaces); % 31xn
 
 % Generate sensors responses from illuminant and surfaces
-sensorRes = (repmat(trueIllum, 1, nSurfaces).*trueSurfArray)' * sensorResCur; % nx3
+sensorRes = (repmat(trueIllum, 1, nSurfaces).*trueSurfA)' * sensorResCur; % nx3
 
 % Run the algorithm
-[estIllum, estSurfArray] = maloneywandell(lightB, surfB, sensorResCur, sensorRes);
+[estIllum, estSurfA] = maloneywandell(lightB, surfB, sensorResCur, sensorRes);
+
+% original lighting and reflectance
+sensorRes = repmat(trueIllum, 1, nSurfaces)' * sensorResCur; % nx3
+surfRes = trueSurfA' * sensorResCur; % nx3
+% recovered lighting and reflectance
+EsensorRes = repmat(estIllum, 1, nSurfaces)' * sensorResCur; % nx3
+EsurfRes = estSurfA' * sensorResCur; % nx3
 
 % plot them
-figure;
+figure('Name','Simulation Results for All Surface Reflectance Recovery Tests');
 for i = 1:nSurfaces
     subplot(3,ceil(nSurfaces/3),i);
-    trueSurf = trueSurfArray(:,i);
-    estSurf = estSurfArray(:,i);
+    trueSurf = trueSurfA(:,i);
+    estSurf = estSurfA(:,i);
     % Plot the true surface reflectance against the estimate
     plot([trueSurf, estSurf]);
-    legend('True','Est');
+    legend('True','Est.');
 end
 
+figure;
+Eim = reshape(sensorRes,[3,ceil(nSurfaces/3),3]);
+Sim = reshape(surfRes,[3,ceil(nSurfaces/3),3]);
+eEim = reshape(EsensorRes,[3,ceil(nSurfaces/3),3]);
+eSim = reshape(EsurfRes,[3,ceil(nSurfaces/3),3]);
+subplot(2,2,1); imshow(Eim); title('original illuminant');
+subplot(2,2,2); imshow(Sim); title('original reflectance');
+subplot(2,2,3); imshow(eEim); title('estimated illuminant');
+subplot(2,2,4); imshow(eSim); title('estimated reflectance');
--- maloneywandell.m
+++ maloneywandell.m
@@ -1,38 +1,47 @@
 function [E, surfArray] = maloneywandell(lightB, surfB, sensorResCur, sensorRes)
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Michael Harris, michael.harris@uea.ac.uk
-    % University of East Anglia, Norwich, UK
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Modified by Han Gong, gong@fedoraproject.org
+% University of East Anglia, Norwich, UK
+% 07/05/2015
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    % modified by Han Gong, gong@fedoraproject.org
-    
-    % Find nullspace of sensor responses, which gives the normal of the
-    % plane in the illuminant space
-    EPlaneNorm = null(sensorRes); % null of a nx3 matrix
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Michael Harris, michael.harris@uea.ac.uk
+% University of East Anglia, Norwich, UK
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    baseSurf = zeros(size(surfB,2), size(sensorResCur,2)); % NxP
-    for i = 1:size(surfB,2)
-        tmp = (repmat(surfB(:,i),1,size(lightB,2)).*lightB)' * sensorResCur; % integrate ESR
-        baseSurf(i,:) = tmp * EPlaneNorm; 
+% Find null space of sensor responses, which gives the normal of the
+% plane in the illuminant space
+[~,~,v] = svd(sensorRes); % null of a nx3 matrix
+EPlaneNorm = v(:,end);
+
+baseSurf = zeros(size(surfB,2), size(lightB,2)); % NxP surface-light base
+for i = 1:size(surfB,2) 
+    % for each type of surface base (assume the weights for the other surfaces are 0)
+    tmp = (repmat(surfB(:,i),1,size(lightB,2)).*lightB)' * sensorResCur; % integrate ESR
+    baseSurf(i,:) = tmp * EPlaneNorm; % dot product of P responces and the plane normal
+end
+
+% The correct epsilon will give near zero dot products for all type of base illuminants.
+% This means that the responses and the normal are indeed orthogonal
+[~,~,v] = svd(baseSurf);
+epsilon = v(:,end);
+
+% The estimate for the illuminant (basis * weights)
+E = lightB * epsilon;
+
+% Create the lighting matrix using the illuminant estimate
+lightM = zeros(size(sensorResCur,2),size(surfB,2)); % \Lambda PxN
+for k = 1:size(sensorResCur,2)
+    for j = 1:size(surfB,2)
+        lightM(k,j) = sum(E.*surfB(:,j).*sensorResCur(:,k)); % integration ESR
     end
-    
-    epsilon = null(baseSurf);
-    
-    % The estimate for the illuminant (basis * weights)
-    E = lightB * epsilon;
+end
 
-    % Create the lighting matrix using the illuminant estimate
-    lightM = zeros(size(lightB,2),size(surfB,2)); % \Lambda PxN
-    for k = 1:size(lightB,2)
-        for j = 1:size(surfB,2)
-            lightM(k,j) = sum(E.*surfB(:,j).*sensorResCur(:,k)); % integration ESR
-        end
-    end
-
-    % Convert sensor responses into sigma values
-    surfArray = zeros(size(surfB, 1), size(sensorRes, 1)); % 31xn
-    for i = 1:size(sensorRes, 1)
-        surfArray(:,i) = surfB * (lightM\sensorRes(i,:)');
-    end
+% Convert sensor responses into sigma values
+surfArray = zeros(size(surfB, 1), size(sensorRes, 1)); % 31xn
+for i = 1:size(sensorRes,1)
+    surfArray(:,i) = surfB * (lightM\sensorRes(i,:)');
+end
 
 end
